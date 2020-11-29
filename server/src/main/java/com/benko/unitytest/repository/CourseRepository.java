66baaa07f82d935d55178e574ab8b2aab3ba3c5a
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2020-11-29 16:19:38 -0500

    Added integration with Keycloak for OAuth 2.0

--- server/src/main/java/com/benko/unitytest/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.benko.unitytest.repository;
-
-import com.benko.unitytest.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-}
