dc614fa06229e7cd820d7a1a9b50b8a97ba931af
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2020-12-08 19:16:16 -0500

    Move Term enum to separate models folder for rest implementation

--- backend/course-management/src/main/java/com/unityTest/courseManagement/restImpl/CourseController.java
+++ backend/course-management/src/main/java/com/unityTest/courseManagement/restImpl/CourseController.java
@@ -2,11 +2,13 @@ package com.unityTest.courseManagement.restImpl;
 
 import com.unityTest.courseManagement.entity.Course;
 import com.unityTest.courseManagement.entity.CourseAttribute;
+import com.unityTest.courseManagement.models.Term;
 import com.unityTest.courseManagement.restApi.CourseApi;
 import com.unityTest.courseManagement.service.CourseService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,7 +29,14 @@ public class CourseController implements CourseApi {
 
     @Override
     public ResponseEntity<List<Course>> getCourses(Integer id, String code, Integer level, String term, Integer year) {
-        return ResponseEntity.ok().body(courseService.getCourses(id, code, level, term, year));
+        // Convert term to Enum before searching
+        Term termEnum = null;
+        if(term != null) {
+            try { termEnum = Term.valueOf(term); }
+            catch(IllegalArgumentException e) { throw new HttpMessageNotReadableException("Not one of accepted values"); }
+        }
+        // Retrieve results using service
+        return ResponseEntity.ok().body(courseService.getCourses(id, code, level, termEnum, year));
     }
 
     @Override
