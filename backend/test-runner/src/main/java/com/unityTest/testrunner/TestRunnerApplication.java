4fe346741acc8858afa35377db7095dce2184ab2
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2021-01-07 16:14:41 -0500

    Create project structure for test runner microservice

---
+++ backend/test-runner/src/main/java/com/unityTest/testrunner/TestRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.unityTest.testrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRunnerApplication.class, args);
+	}
+
+}
---
+++ backend/test-runner/src/test/java/com/unityTest/testrunner/TestRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unityTest.testrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
