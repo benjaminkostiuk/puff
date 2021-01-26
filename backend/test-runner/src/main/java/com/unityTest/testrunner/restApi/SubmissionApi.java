3286fcc9419f6bb1eeace2ddf1ca9ee04d8b826c
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2021-01-25 21:09:21 -0500

    Update submissions API for Multipart file and add exception signature

--- backend/test-runner/src/main/java/com/unityTest/testrunner/restApi/SubmissionApi.java
+++ backend/test-runner/src/main/java/com/unityTest/testrunner/restApi/SubmissionApi.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Api(value = "Submissions API", tags = "Submissions API", description = "Upload source files to run against test cases")
 public interface SubmissionApi extends BaseApi {
 
@@ -18,5 +20,5 @@ public interface SubmissionApi extends BaseApi {
      */
     @ApiOperation(value = "Upload source code files", nickname = "uploadSourceFiles", response = String.class, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<String> uploadSourceFiles(@ApiParam(name = "files", required = true) @RequestParam("files") MultipartFile[] files);
+    ResponseEntity<String> uploadSourceFiles(@ApiParam(name = "files", required = true) @RequestParam("files") MultipartFile[] files) throws IOException;
 }
--- backend/test-runner/src/main/java/com/unityTest/testrunner/restImpl/SubmissionController.java
+++ backend/test-runner/src/main/java/com/unityTest/testrunner/restImpl/SubmissionController.java
@@ -1,14 +1,41 @@
 package com.unityTest.testrunner.restImpl;
 
+import com.unityTest.testrunner.entity.SourceFile;
+import com.unityTest.testrunner.repository.SourceFileRepository;
 import com.unityTest.testrunner.restApi.SubmissionApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 public class SubmissionController implements SubmissionApi {
+    @Autowired
+    private SourceFileRepository sourceFileRepository;
+
     @Override
-    public ResponseEntity<String> uploadSourceFiles(MultipartFile[] files) {
-        return null;
+    public ResponseEntity<String> uploadSourceFiles(MultipartFile[] files) throws IOException {
+        if(files.length == 0) {
+            // Throw exception here
+        }
+        for(MultipartFile file : files) {
+            // Create a new source file and save
+            System.out.println(file.getName());
+            try {
+                SourceFile sourceFile = new SourceFile();
+                sourceFile.setSubmissionId(2);
+                sourceFile.setAuthorId("testing");
+                sourceFile.setContent(file.getBytes());
+                sourceFileRepository.save(sourceFile);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return new ResponseEntity<>("File(s) saved!", HttpStatus.OK);
     }
 }
