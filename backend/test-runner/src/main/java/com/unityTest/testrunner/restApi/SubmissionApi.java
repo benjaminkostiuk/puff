d57afd245cb156b3aee3026c88f0debb8a5bd99c
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2021-02-15 20:26:23 -0500

    Update /upload endpoint
    - Added exception handling for saving files with new exceptions EmptyFileException and NoFilesUploadedException
    - Implemented use of new submission response model
    - Implemented logic for saving data to a SourceFile table

--- backend/test-runner/src/main/java/com/unityTest/testrunner/restApi/SubmissionApi.java
+++ backend/test-runner/src/main/java/com/unityTest/testrunner/restApi/SubmissionApi.java
@@ -1,15 +1,20 @@
 package com.unityTest.testrunner.restApi;
 
+import com.unityTest.testrunner.models.response.Submission;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
 
 @Api(value = "Submissions API", tags = "Submissions API", description = "Upload source files to run against test cases")
 public interface SubmissionApi extends BaseApi {
@@ -18,7 +23,16 @@ public interface SubmissionApi extends BaseApi {
      * POST endpoint to upload source code files
      * @return TODO figure this out
      */
-    @ApiOperation(value = "Upload source code files", nickname = "uploadSourceFiles", response = String.class, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Upload source code files", nickname = "uploadSourceFiles", response = Submission.class, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<String> uploadSourceFiles(@ApiParam(name = "files", required = true) @RequestParam("files") MultipartFile[] files) throws IOException;
+    ResponseEntity<Submission> uploadSourceFiles(
+            @ApiParam(name = "files", required = true) @RequestParam("files") MultipartFile[] files,
+            @ApiParam(name = "assignmentId", required = true) @RequestParam("assignmentId") Integer assignmentId) throws IOException;
+
+    @ApiOperation(value = "Download source code upload", nickname = "downloadSourceFiles")
+    @GetMapping(value = "/download", produces = "application/zip")
+    void downloadSourceFiles(
+            OutputStream out
+            //@ApiParam(name = "Submission id", required = true) @PathVariable(value = "submissionId") Integer submissionId
+    ) throws FileNotFoundException;
 }
--- backend/test-runner/src/main/java/com/unityTest/testrunner/restImpl/SubmissionController.java
+++ backend/test-runner/src/main/java/com/unityTest/testrunner/restImpl/SubmissionController.java
@@ -1,41 +1,116 @@
 package com.unityTest.testrunner.restImpl;
 
 import com.unityTest.testrunner.entity.SourceFile;
+import com.unityTest.testrunner.exception.EmptyFileException;
+import com.unityTest.testrunner.exception.NoFilesUploadedException;
+import com.unityTest.testrunner.models.response.Submission;
 import com.unityTest.testrunner.repository.SourceFileRepository;
 import com.unityTest.testrunner.restApi.SubmissionApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Date;
+import java.util.Optional;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
+/**
+ * Rest Controller for the /upload and /download endpoints
+ */
 @RestController
 public class SubmissionController implements SubmissionApi {
+
     @Autowired
     private SourceFileRepository sourceFileRepository;
 
     @Override
-    public ResponseEntity<String> uploadSourceFiles(MultipartFile[] files) throws IOException {
-        if(files.length == 0) {
-            // Throw exception here
+    public ResponseEntity<Submission> uploadSourceFiles(MultipartFile[] files, Integer assignmentId) throws IOException {
+        // Check the number of files uploaded
+        if(files == null || files.length == 0) {
+            throw new NoFilesUploadedException();
         }
+        // Obtain a new submission id to group files by
+        Integer submissionId = sourceFileRepository.nextSubmissionId();
+        Date submissionDate = new Date();
+        // TODO Get the author id
+        String authorId = "";
+        // Create response for upload endpoint
+        Submission submission = new Submission(submissionId, assignmentId, submissionDate);
+
+        // Save files to repo
         for(MultipartFile file : files) {
-            // Create a new source file and save
-            System.out.println(file.getName());
-            try {
-                SourceFile sourceFile = new SourceFile();
-                sourceFile.setSubmissionId(2);
-                sourceFile.setAuthorId("testing");
-                sourceFile.setContent(file.getBytes());
-                sourceFileRepository.save(sourceFile);
-            } catch(Exception e) {
-                e.printStackTrace();
+            // Check that file is not empty
+            if(file.isEmpty()) {
+                throw new EmptyFileException(file.getOriginalFilename());
             }
+            // Create a new source file and save it to the repository
+            SourceFile sourceFile = new SourceFile(0, submissionId, assignmentId, file.getOriginalFilename(), submissionDate, "TODO ADD HERE", file.getBytes());
+            sourceFileRepository.save(sourceFile);
 
+            // Add saved file to submission response
+            submission.addSourceFile(file.getOriginalFilename(), file.getSize());
+        }
+        return new ResponseEntity<>(submission, HttpStatus.OK);
+    }
+
+    @Override
+    public void downloadSourceFiles(OutputStream out) throws FileNotFoundException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=\"test.zip\"");
+        Optional<SourceFile> sf = sourceFileRepository.findById(1000);
+
+        SourceFile sourceFile = sf.get();
+
+        // Write to the file
+        File file = new File("filename.hs");
+
+
+        try(FileOutputStream outputStream = new FileOutputStream(String.valueOf(file))) {
+            outputStream.write(sourceFile.getContent());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return new ResponseEntity<>("File(s) saved!", HttpStatus.OK);
+
+
+        // create a list to add files to be zipped
+//        ArrayList<File> files = new ArrayList<>(2);
+//        files.add(new File("README.md"));
+//
+//        // package files
+//        for (File file : files) {
+//            //new zip entry and copying inputstream with file to zipOutputStream, after all closing streams
+//            zipOutputStream.putNextEntry(new ZipEntry(file.getName()));
+//            FileInputStream fileInputStream = new FileInputStream(file);
+//
+//            IOUtils.copy(fileInputStream, zipOutputStream);
+//
+//            fileInputStream.close();
+//            zipOutputStream.closeEntry();
+//        }
+        FileSystemResource resource = new FileSystemResource(file);
+        try (ZipOutputStream zippedOut = new ZipOutputStream(out)) {
+            ZipEntry e = new ZipEntry(resource.getFilename());
+            // Configure the zip entry, the properties of the file
+
+            e.setSize(resource.contentLength());
+            e.setTime(System.currentTimeMillis());
+            // etc.
+            zippedOut.putNextEntry(e);
+            // And the content of the resource:
+            StreamUtils.copy(resource.getInputStream(), zippedOut);
+            zippedOut.closeEntry();
+            zippedOut.finish();
+        } catch (Exception e) {
+            // Do something with Exception
+            e.printStackTrace();
+        }
     }
 }
