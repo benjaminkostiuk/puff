dbced89b0bca154dff3af59faf5e037632b55d5e
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2021-02-16 21:10:41 -0500

    Implement /download endpoint
    - Implement zip download logic in rest controller for /download endpoint
    - Add zip download for all files with submission id

--- backend/test-runner/src/main/java/com/unityTest/testrunner/restApi/SubmissionApi.java
+++ backend/test-runner/src/main/java/com/unityTest/testrunner/restApi/SubmissionApi.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -21,7 +22,7 @@ public interface SubmissionApi extends BaseApi {
 
     /**
      * POST endpoint to upload source code files
-     * @return TODO figure this out
+     * @return Submission object as confirmation with file information
      */
     @ApiOperation(value = "Upload source code files", nickname = "uploadSourceFiles", response = Submission.class, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -29,10 +30,13 @@ public interface SubmissionApi extends BaseApi {
             @ApiParam(name = "files", required = true) @RequestParam("files") MultipartFile[] files,
             @ApiParam(name = "assignmentId", required = true) @RequestParam("assignmentId") Integer assignmentId) throws IOException;
 
-    @ApiOperation(value = "Download source code upload", nickname = "downloadSourceFiles")
+    /**
+     * GET endpoint to download files uploaded by a submission as a zip
+     * @param submissionId Id of submission to search for, downloading all source files uploaded by that submission
+     */
+    @ApiOperation(value = "Download source code upload", nickname = "downloadSourceFiles", produces = "application/zip")
     @GetMapping(value = "/download", produces = "application/zip")
     void downloadSourceFiles(
-            OutputStream out
-            //@ApiParam(name = "Submission id", required = true) @PathVariable(value = "submissionId") Integer submissionId
-    ) throws FileNotFoundException;
+            HttpServletResponse response,
+            @ApiParam(name = "Submission id", required = true) @RequestParam(value = "submissionId") Integer submissionId) throws IOException;
 }
--- backend/test-runner/src/main/java/com/unityTest/testrunner/restImpl/SubmissionController.java
+++ backend/test-runner/src/main/java/com/unityTest/testrunner/restImpl/SubmissionController.java
@@ -6,6 +6,7 @@ import com.unityTest.testrunner.exception.NoFilesUploadedException;
 import com.unityTest.testrunner.models.response.Submission;
 import com.unityTest.testrunner.repository.SourceFileRepository;
 import com.unityTest.testrunner.restApi.SubmissionApi;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
@@ -15,8 +16,11 @@ import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -36,6 +40,8 @@ public class SubmissionController implements SubmissionApi {
         if(files == null || files.length == 0) {
             throw new NoFilesUploadedException();
         }
+        // TODO Add limit of 10 files for uploads
+
         // Obtain a new submission id to group files by
         Integer submissionId = sourceFileRepository.nextSubmissionId();
         Date submissionDate = new Date();
@@ -61,56 +67,31 @@ public class SubmissionController implements SubmissionApi {
     }
 
     @Override
-    public void downloadSourceFiles(OutputStream out) throws FileNotFoundException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Disposition", "attachment; filename=\"test.zip\"");
-        Optional<SourceFile> sf = sourceFileRepository.findById(1000);
+    public void downloadSourceFiles(HttpServletResponse response, Integer submissionId) throws IOException {
+        final String ZIP_NAME = "submission.zip";
 
-        SourceFile sourceFile = sf.get();
+        // Set response headers
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.addHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", ZIP_NAME));
 
-        // Write to the file
-        File file = new File("filename.hs");
+        // Get SourceFiles from submission
+        List<SourceFile> files = sourceFileRepository.getSourceFilesBySubmissionId(submissionId);
 
+        // Create output stream to stream zipped files
+        ZipOutputStream zipOutputStream = new ZipOutputStream(response.getOutputStream());
+        for(SourceFile file : files) {
+            // Create new entry for file
+            zipOutputStream.putNextEntry(new ZipEntry(file.getFileName()));
+            // Create byte stream to send file contents to zip stream
+            // This keeps all content IN MEMORY so files cannot be too large
+            ByteArrayInputStream byteArrayStream = new ByteArrayInputStream(file.getContent());
+            IOUtils.copy(byteArrayStream, zipOutputStream);
 
-        try(FileOutputStream outputStream = new FileOutputStream(String.valueOf(file))) {
-            outputStream.write(sourceFile.getContent());
-        } catch (IOException e) {
-            e.printStackTrace();
+            // Close byte stream and entry in zip
+            byteArrayStream.close();
+            zipOutputStream.closeEntry();
         }
-
-
-
-        // create a list to add files to be zipped
-//        ArrayList<File> files = new ArrayList<>(2);
-//        files.add(new File("README.md"));
-//
-//        // package files
-//        for (File file : files) {
-//            //new zip entry and copying inputstream with file to zipOutputStream, after all closing streams
-//            zipOutputStream.putNextEntry(new ZipEntry(file.getName()));
-//            FileInputStream fileInputStream = new FileInputStream(file);
-//
-//            IOUtils.copy(fileInputStream, zipOutputStream);
-//
-//            fileInputStream.close();
-//            zipOutputStream.closeEntry();
-//        }
-        FileSystemResource resource = new FileSystemResource(file);
-        try (ZipOutputStream zippedOut = new ZipOutputStream(out)) {
-            ZipEntry e = new ZipEntry(resource.getFilename());
-            // Configure the zip entry, the properties of the file
-
-            e.setSize(resource.contentLength());
-            e.setTime(System.currentTimeMillis());
-            // etc.
-            zippedOut.putNextEntry(e);
-            // And the content of the resource:
-            StreamUtils.copy(resource.getInputStream(), zippedOut);
-            zippedOut.closeEntry();
-            zippedOut.finish();
-        } catch (Exception e) {
-            // Do something with Exception
-            e.printStackTrace();
-        }
+        zipOutputStream.close();        // Close zip stream
+        response.flushBuffer();         // Flush response buffer
     }
 }
