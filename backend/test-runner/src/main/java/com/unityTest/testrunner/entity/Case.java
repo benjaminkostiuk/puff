c8d46ff12857f07f3563111e59de88c1216ad4a4
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2021-04-28 20:22:22 -0400

    Create API models for test case endpoints
    - Refactor case entity to join with Suite
    - Create new functions to map to TestCase class simply
    - Create new TestCaseInfo class for building cases from apis
    - Create new TestCase class for displaying test cases, associated class
    include Author, CaseStats, and TestCasePage

--- backend/test-runner/src/main/java/com/unityTest/testrunner/entity/Case.java
+++ backend/test-runner/src/main/java/com/unityTest/testrunner/entity/Case.java
@@ -1,6 +1,9 @@
 package com.unityTest.testrunner.entity;
 
 import com.unityTest.testrunner.models.PLanguage;
+import com.unityTest.testrunner.models.response.Author;
+import com.unityTest.testrunner.models.response.CaseStats;
+import com.unityTest.testrunner.models.response.TestCase;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -32,7 +35,7 @@ public class Case {
     private int id;
 
     // Test suite that case belongs to
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "SUITE_ID", referencedColumnName = "ID", nullable = false)
     private Suite suite;
 
@@ -50,7 +53,7 @@ public class Case {
 
     // Number of upvotes for test case, needed for easy sorting & pagination when pulling test cases
     @Column(name = "UPVOTE_COUNT")
-    private int upvoteCount;
+    private int upvotes;
 
     @Column(name = "RUN_COUNT")
     private int runCount;
@@ -64,4 +67,27 @@ public class Case {
 
     @Column(name = "CODE")
     private String code;
+
+    public Case(Suite suite, String authorId, String functionName, String description, PLanguage language, String code) {
+        this(0, suite, authorId, functionName, description, 0, 0, 0, language, code);
+    }
+
+    /**
+     * Convert case to TestCase Api response object
+     * @param author Author object to set in TestCase
+     * @return TestCase response object with author
+     */
+    public TestCase toTestCase(Author author) {
+        TestCase testCase = this.toTestCase();
+        testCase.setAuthor(author);
+        return testCase;
+    }
+
+    /**
+     * Convert case to TestCase Api response object
+     * @return TestCase response object
+     */
+    public TestCase toTestCase() {
+        return new TestCase(this.id, this.suite, this.description, this.language, null, this.upvotes, new CaseStats(this.runCount, this.passCount), this.code);
+    }
 }
--- backend/test-runner/src/main/java/com/unityTest/testrunner/models/page/TestCasePage.java
+++ backend/test-runner/src/main/java/com/unityTest/testrunner/models/page/TestCasePage.java
@@ -1,12 +1,40 @@
 package com.unityTest.testrunner.models.page;
 
 import com.unityTest.testrunner.entity.Case;
+import com.unityTest.testrunner.models.response.Author;
+import com.unityTest.testrunner.models.response.TestCase;
 import io.swagger.annotations.ApiModel;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.data.domain.Page;
 
-@ApiModel(value = "CasePage", description = "Page request for test cases")
-public class CasePage extends BasePage<Case> {
-    public CasePage(Page<Case> page) {
-        super(page);
+@ApiModel(value = "TestCasePage", description = "Page request for test cases")
+public class TestCasePage extends BasePage<TestCase> {
+    public TestCasePage(Page<Case> page) {
+        super(page.map(Case::toTestCase));
+    }
+
+    /**
+     * TestCasePage constructor
+     * @param page Page of cases
+     * @param keycloak Keycloak api connection
+     */
+    public TestCasePage(Page<Case> page, Keycloak keycloak) {
+        super(page.map(caze -> {
+            // Get user representation with id from keycloak connection
+            UserRepresentation user = keycloak.realm("puff").users().get(caze.getAuthorId()).toRepresentation();
+            // Create author object with the user representation
+            Author author = new Author(user.getFirstName(), user.getLastName());
+            return caze.toTestCase(author);
+        }));
+    }
+
+    /**
+     * TestCasePage constructor
+     * @param page Page of cases
+     * @param author Author of all cases
+     */
+    public TestCasePage(Page<Case> page, Author author) {
+        super(page.map(caze -> caze.toTestCase(author)));
     }
 }
