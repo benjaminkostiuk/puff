01a467ca0d01d18bbcb922a8e17b923fb72ef6f3
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2021-04-30 11:49:09 -0400

    Add async config and test functions in code service
    - Create async config to handle asynchronus thread spawning
    - Create test functions in CodeService to test streaming
    - Create new model ResultStatus and TestResult to return results

---
+++ backend/test-runner/src/main/java/com/unityTest/testrunner/models/ResultStatus.java
@@ -0,0 +1,8 @@
+package com.unityTest.testrunner.models;
+
+public enum ResultStatus {
+    PASS,
+    FAIL,
+    COMPILATION_ERROR,
+    RUNTIME_ERROR
+}
--- backend/test-runner/src/main/java/com/unityTest/testrunner/service/CodeService.java
+++ backend/test-runner/src/main/java/com/unityTest/testrunner/service/CodeService.java
@@ -1,13 +1,51 @@
 package com.unityTest.testrunner.service;
 
+import com.unityTest.testrunner.entity.Case;
+import com.unityTest.testrunner.entity.Submission;
+import com.unityTest.testrunner.entity.Suite;
 import com.unityTest.testrunner.exception.code.InvalidFunctionName;
 import com.unityTest.testrunner.models.PLanguage;
+import com.unityTest.testrunner.models.ResultStatus;
+import com.unityTest.testrunner.models.response.TestResult;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
+import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 @Service
 public class CodeService {
+
+    @Value("${code-runner.dir.path}")
+    private String workingDirectoryPath;
+
+    @Async("threadPoolTaskExecutor")
+    public void runTestCasesInSuite(ResponseBodyEmitter emitter, Submission submission, Suite suite, List<Case> testCases) {
+
+    }
+
+
+    @Async("threadPoolTaskExecutor")
+    public void asyncDoSomething(ResponseBodyEmitter emitter) throws IOException, InterruptedException {
+        System.out.println("Starting asyncDoSomething on thread " + Thread.currentThread().getName());
+        for(int i = 0; i <= 10; i++) {
+            TestResult result = new TestResult(i, ResultStatus.PASS, "testing");
+            emitter.send(result, MediaType.APPLICATION_JSON);
+            Thread.sleep(1000);
+        }
+        this.asyncRun();
+        System.out.println("Now on" + Thread.currentThread().getName());
+        emitter.complete();
+    }
+
+    @Async("threadPoolTaskExecutor")
+    public void asyncRun() {
+        System.out.println("Starting asyncRun on thread " + Thread.currentThread().getName());
+    }
 
     public String buildTestCaseCode(PLanguage lang, String functionName, String functionBody) {
         // Check for invalid characters in the function name
